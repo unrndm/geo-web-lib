f673cc802faac5179e49b157d36034c64711ebbc
unrndm <unrndm@gmail.com>
2023-11-02 05:07:45 +0300

    feat: ironed out some bugs

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,11 @@
-// mod geomdiff;
 mod rasterize;
 mod utils;
 
 use rasterize::Rasterize;
-// use std::collections::HashMap;
 
-// use geo::algorithm::BooleanOps;
-use geo_types::{Geometry, LineString};
+use geo_types::LineString;
 use wasm_bindgen::prelude::*;
-use wkt::{ToWkt, TryFromWkt};
+use wkt::TryFromWkt;
 
 #[wasm_bindgen]
 extern "C" {
@@ -17,24 +14,11 @@ extern "C" {
 }
 
 macro_rules! console_log {
-  // Note that this is using the `log` function imported above during
-  // `bare_bones`
   ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
 }
 
-// #[derive(Debug)]
-// pub enum Data {
-//     Vector(String),
-//     // RasterF64 {
-//     //     value: Box<[f64]>,
-//     //     shape: Box<[i64]>,
-//     // },
-// }
-
 #[wasm_bindgen]
-pub struct Calculator {
-    // data: HashMap<String, Data>,
-}
+pub struct Calculator {}
 
 #[wasm_bindgen]
 impl Calculator {
@@ -42,41 +26,11 @@ impl Calculator {
     pub fn new() -> Calculator {
         utils::set_panic_hook();
 
-        Calculator {
-        //     data: HashMap::new(),
-        }
+        Calculator {}
     }
 
-    pub fn rasterize(&self, simple_line_wkt: &str) -> Box<[u8]> {
-        console_log!("{:#?}", simple_line_wkt);
-        let line: LineString<i64> = LineString::try_from_wkt_str(simple_line_wkt).unwrap_throw();
+    pub fn rasterize(&self, simple_line_wkt: &str) -> Box<[i32]> {
+        let line: LineString<i32> = LineString::try_from_wkt_str(simple_line_wkt).unwrap_throw();
         line.rasterize()
     }
-
-    // pub fn add_variable_from_wkt(&mut self, name: String, wkt: String) {
-    //     self.data.insert(name, Data::Vector(wkt));
-    // }
-
-    // pub fn calculate(&self, operation: &str) -> Result<String, String> {
-    //     console_log!("{:#?}", self.data);
-    //     match operation {
-    //         // "intersection" => match (self.data.get("A"), self.data.get("B")) {
-    //         //     (Some(Data::Vector(a)), Some(Data::Vector(b))) => {
-    //         //         let a_geom: Geometry<f64> = Geometry::try_from_wkt_str(a).unwrap();
-    //         //         let b_geom: Geometry<f64> = Geometry::try_from_wkt_str(b).unwrap();
-    //         //         Ok(a_geom.intersection(&b_geom).to_wkt().to_string())
-    //         //     }
-    //         //     _ => Err("not enough variable defined".to_string()),
-    //         // },
-    //         "geomdiff" => match (self.data.get("A"), self.data.get("B")) {
-    //             (Some(Data::Vector(a)), Some(Data::Vector(b))) => {
-    //                 let a_geom: Point<f64> = Point::try_from_wkt_str(a).unwrap();
-    //                 let b_geom: Point<f64> = Point::try_from_wkt_str(b).unwrap();
-    //                 Ok(format!("{:#?}", a_geom.diff(b_geom.into())).to_string())
-    //             }
-    //             _ => Err("not enough variable defined".to_string()),
-    //         },
-    //         _ => Err("unknown operation".to_string()),
-    //     }
-    // }
 }
--- src/rasterize.rs
+++ src/rasterize.rs
@@ -1,57 +1,87 @@
-use geo::GeoNum;
+use super::log;
+
+macro_rules! console_log {
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
+}
+
 use geo_types::{Coord, Line, LineString};
 
 pub trait Rasterize: Sized {
-    fn rasterize(&self) -> Box<[u8]>;
+    fn rasterize(&self) -> Box<[i32]>;
 }
 
-impl Rasterize for LineString<i64> {
-    fn rasterize(&self) -> Box<[u8]> {
-        let min_x = self
-            .into_iter()
-            .map(|coord| coord.x)
-            // .fold(f64::NAN, f64::min);
-            .fold(i64::MAX, i64::min);
-        let max_x = self
-            .into_iter()
-            .map(|coord| coord.x)
-            .fold(i64::MIN, i64::max);
-        let min_y = self
-            .into_iter()
-            .map(|coord| coord.y)
-            .fold(i64::MAX, i64::min);
-        let max_y = self
-            .into_iter()
-            .map(|coord| coord.y)
-            .fold(i64::MIN, i64::max);
+impl Rasterize for LineString<i32> {
+    fn rasterize(&self) -> Box<[i32]> {
+        console_log!("self: {:#?}", self);
 
-        let offset: Coord<i64> = (min_x, max_y).into();
+        let min_x = self.into_iter().map(|coord| coord.x).min().unwrap();
+        let max_x = self.into_iter().map(|coord| coord.x).max().unwrap();
+        let min_y = self.into_iter().map(|coord| coord.y).min().unwrap();
+        let max_y = self.into_iter().map(|coord| coord.y).max().unwrap();
 
-        let mut res = vec![0; ((max_x - min_x) * (max_y - min_y)) as usize];
+        let offset: Coord<i32> = (min_x, min_y).into();
 
-        for line in self.lines() {
-            // line = line
-            let (xi, dx) = if line.dx() < 0 {
-                (-1, -line.dx())
+        let width = max_x - min_x;
+        let height = max_y - min_y;
+
+        let self_with_offset: LineString<i32> =
+            self.0.iter().map(|coord| *coord - offset).collect();
+
+        console_log!("self with offset: {:#?}", self_with_offset);
+
+        let mut res = vec![0; (width * height) as usize];
+
+        for line in self_with_offset.lines() {
+            console_log!("line: {:#?}", line);
+
+            // correct_line with correct slope
+            // maybe use `.slope`?
+            let corrected_line =
+                if (line.end.y - line.start.y).abs() < (line.end.x - line.start.x).abs() {
+                    if line.start.x > line.start.x {
+                        Line::new(line.end, line.start)
+                        // line
+                    } else {
+                        line
+                    }
+                } else {
+                    if line.start.y > line.end.y {
+                        Line::new(line.end, line.start)
+                    } else {
+                        line
+                    }
+                };
+
+            console_log!("correct line: {:#?}", corrected_line);
+
+            let (xi, dx) = if corrected_line.dx() < 0 {
+                (-1, -corrected_line.dx())
             } else {
-                (1, line.dx())
+                (1, corrected_line.dx())
             };
-            let mut D = (2 * dx) - line.dy();
+            let mut D = (2 * dx) - corrected_line.dy();
 
-            let mut x = line.start.x;
+            let mut x = corrected_line.start.x;
 
-            for y in line.start.y..line.end.y {
-                res[((max_x - min_x) * (x - min_x) + (y - min_y)) as usize] = 1;
+            for y in corrected_line.start.y..corrected_line.end.y {
+                res[(width * x + y) as usize] = 255;
                 D = if D > 0 {
                     x += xi;
-                    D + (2 * (dx - line.dy()))
+                    D + (2 * (dx - corrected_line.dy()))
                 } else {
                     D + 2 * dx
                 }
             }
         }
 
-        res.append(&mut vec![(max_x - min_x) as u8, (max_y - min_y) as u8]);
+        res = res
+            .iter()
+            .map(|val| [*val, *val, *val, *val])
+            .flatten()
+            .collect::<Vec<_>>();
+
+        res.insert(0, (max_x - min_x) as i32);
+        res.insert(0, (max_y - min_y) as i32);
         res.into_boxed_slice()
     }
 }
